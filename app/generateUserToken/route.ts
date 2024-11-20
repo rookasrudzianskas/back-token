91502ddd12760db89572e02b9ba2a6b90cfb9fe3
rokas <rookas.rudzenskas@gmail.com>
2024-11-20 21:04:31 +0100

    fix

--- app/generateUserToken/route.ts
+++ app/generateUserToken/route.ts
@@ -1,4 +1,5 @@
-import { StreamClient } from '@stream-io/node-sdk';
+import {StreamClient, UserRequest} from '@stream-io/node-sdk';
+import {NextResponse} from "next/server";
 
 const apiKey = process.env.API_GET_STREAM_PUBLISHABLE_KEY;
 const secret = process.env.API_GET_STREAM_SECRET_KEY;
@@ -7,11 +8,34 @@ if(!apiKey || !secret) {
   return res.status(500).json({ error: "Missing API Key" });
 }
 
+const client = new StreamClient(apiKey, secret);
+
 export async function POST(req: Request, res: Response) {
   const { userId, name, image, email } = await req.json();
 
-  client = new StreamClient(apiKey, secret);
+  const newUser: UserRequest = {
+    id: userId,
+    role: 'user',
+    name,
+    image,
+    custom: {
+      email,
+    }
+  };
 
-  client = new StreamClient(apiKey, secret, { timeout: 3000 });
+  await client.upsertUsers([newUser]);
+
+  const validity = 60 * 60 * 24 * 7;
+
+  const token = client.generateUserToken({
+    user_id: userId,
+    validity_in_seconds: validity,
+  });
+
+  console.log("Generating token for user:", userId, 'and validity:', validity);
+
+  return NextResponse.json({
+    token
+  });
 
 }
